778ab8cfd35dcc151f40551e8c62a8df71a2bcb0
Kalai Ramea <kalai.ramea@gmail.com>
2022-01-10 13:04:39 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AIBEDO'
+copyright = '2021, Ramea'
+author = 'Ramea'
 
 release = '0.1'
 version = '0.1.0'
