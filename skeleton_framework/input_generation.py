e071e8d7aaeeaf76672477c1dde9c5964ebe568f
Ubuntu <ubuntu@ip-172-31-22-125.us-east-2.compute.internal>
2022-03-22 02:34:34 +0000

    misc

--- skeleton_framework/input_generation.py
+++ skeleton_framework/input_generation.py
@@ -2,8 +2,10 @@ import numpy as np
 import os
 from data_loader import load_ncdf, normalize, load_ncdf_to_SphereIcosahedral
 
-lon_list, lat_list, dataset = load_ncdf_to_SphereIcosahedral('/home/ubuntu/Exp3_CESM2_r1i1p1f1_historical_Input.nc', glevel=1)
 
-np.save("test.npy", dataset)
+in_vars = ['cres_pre', 'crel_Pre', 'netTOAcs_Pre', 'clivi_pre', 'clwvi_pre','lcloud_pre', 'cresSurf_Pre', 'crelSurf_Pre', 'netSurfcs_Pre']
+lon_list, lat_list, dataset = load_ncdf_to_SphereIcosahedral('/home/ubuntu/Exp7_CESM2_r1i1p1f1_historical_Input.nc', glevel=3, dvar=in_vars)
+
+np.save("Exp7_CESM2_r1i1p1f1_historical_Input_3.npy", dataset)
 
 
