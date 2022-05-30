c85782512aebfc3105d28a8a9cbc63bea0ff6524
Kalai Ramea <kramea@parc.com>
2022-05-30 11:16:12 -0700

    precip

--- skeleton_framework/sunet_compress_gpu_loss.py
+++ skeleton_framework/sunet_compress_gpu_loss.py
@@ -177,7 +177,7 @@ def main(parser_args):
     def trainer(engine, batch):
         unet.train()
         data_in_initial, data_out = batch
-        print("input", data_in.shape)
+        print("input", data_in_initial.shape)
         #print("output", data_out_initial.shape)
         data_in = data_in_initial[:, :, 0:7]
         #data_in, data_out, data_mean, data_std = batch
