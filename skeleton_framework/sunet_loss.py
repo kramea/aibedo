71d4912396105c621e808f3b680381bcb9b0e21e
Kalai Ramea <kramea@parc.com>
2022-05-30 13:18:50 -0700

    clone tensor data in

--- skeleton_framework/sunet_loss.py
+++ skeleton_framework/sunet_loss.py
@@ -193,12 +193,13 @@ def main(parser_args):
         data_in_initial, data_out = batch
         #print("input", data_in_initial.shape)
         # print("output", data_out_initial.shape)
-        data_in = data_in[:, :, 0:7].clone()
+        data_in = data_in_initial[:, :, 0:7].detach().clone()
         # print("input revised", data_in.shape)
         # data_in, data_out, data_mean, data_std = batch
         # data_out = data_out_initial[:, :, 0:3]
         # data_mean = data_in_initial[:, :, 7:10]
         # data_std = data_in_initial[:, :, 10:]
+        data_in = torch.Tensor(data_in)
         data_in = data_in.to(device)
         data_out = data_out.to(device)
         # data_mean = data_mean.to(device)
