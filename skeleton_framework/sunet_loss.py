dbfede2a2ac8314add385b25a89775dbce070ed2
Kalai Ramea <kramea@parc.com>
2022-05-30 13:39:45 -0700

    clone tensor data in

--- skeleton_framework/sunet_loss.py
+++ skeleton_framework/sunet_loss.py
@@ -34,7 +34,7 @@ def sunet_collate(batch):
     batchShape = batch[0].shape
     varlimit = batchShape[1] - 3  # 3 output variables: tas, psl, pr, 3 mean, 3 std
 
-    data_in_array = np.array([item[:, 0:varlimit-6] for item in batch])  # includes mean and std
+    data_in_array = np.array([item[:, 0:varlimit] for item in batch])  # includes mean and std
     # data_out_array = np.array([item[:, varlimit:] for item in batch])
     data_out_array = np.array([item[:, varlimit:] for item in batch])
     #data_mean_array = np.array([item[:, varlimit-6:varlimit-3] for item in batch])
@@ -181,8 +181,9 @@ def main(parser_args):
 
     def custom_prepare_batch(batch):
         x, y = batch
+        x1 = x[:, :, 0:7]
         return (
-            convert_tensor(x),
+            convert_tensor(x1),
             convert_tensor(y),
         )
 
