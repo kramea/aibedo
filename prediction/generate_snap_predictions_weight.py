6f2213d3de3cbb7ec8ee59443974a6ccc4c82af3
Kalai Ramea <kramea@parc.com>
2022-06-16 22:32:43 -0700

    pred python code

--- prediction/generate_snap_predictions_weight.py
+++ prediction/generate_snap_predictions_weight.py
@@ -1,13 +1,14 @@
 import numpy as np
 import xarray as xr
 import torch
+from spherical_unet.models.spherical_unet.unet_model import SphericalUNet
 from pathlib import Path
 import re
 import time
 
 
-input_file = '/Users/kramea/Documents/AIBEDO_dir/data_aibedo/compress.isosph.CESM2.historical.r1i1p1f1.Input.Exp8_fixed.nc'
-output_file = '/Users/kramea/Documents/AIBEDO_dir/data_aibedo/compress.isosph.CESM2.historical.r1i1p1f1.Output.nc'
+input_file = 'compress.isosph.CESM2.historical.r1i1p1f1.Input.Exp8_fixed.nc'
+output_file = 'compress.isosph.CESM2.historical.r1i1p1f1.Output.nc'
 
 inDS = xr.open_dataset(input_file)
 outDS = xr.open_dataset(output_file)
@@ -16,11 +17,11 @@ n_pixels = len(inDS.ncells)
 in_vars = [ 'crelSurf_pre', 'crel_pre', 'cresSurf_pre', 'cres_pre', 'netTOAcs_pre', 'lsMask', 'netSurfcs_pre']
 out_vars = ['tas_pre', 'psl_pre', 'pr_pre']
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-unet = torch.load('/Users/kramea/Documents/AIBEDO_dir/data_aibedo/unet_model_4.pt',
-                        map_location=torch.device('cpu'))
-unet = unet.module.to(device)
-unet.eval()
+weights_file = torch.load('sunet_state_6.pt', 
+                          map_location=torch.device('cpu'))
+weights_file = {key.replace("module.", ""): value for key, value in weights_file.items()}
+
+unet = SphericalUNet('icosahedron', n_pixels, 6, 'combinatorial', 3, 7, 3)
 
 modelfilename = Path(output_file).stem
 p = re.compile('compress.isosph.(.*).historical.r1i1p1f1.Output')
@@ -38,6 +39,8 @@ for var in out_vars:
     data_all.append(temp_data)
 dataset_out = np.concatenate(data_all, axis=2)
 
+unet.load_state_dict(weights_file, strict=False)
+unet.eval()
 
 inPredict = dataset_in[0:1]
 outPredict = dataset_out[0:1]
@@ -46,8 +49,7 @@ preds = unet(torch.Tensor(inPredict))
 
 pred_numpy = preds.detach().cpu().numpy()
 
-np.save(("/Users/kramea/Documents/AIBEDO_dir/data_aibedo/"+ modelname + "_predictions.npy"), pred_numpy)
-np.save(("/Users/kramea/Documents/AIBEDO_dir/data_aibedo/" + modelname + "_groundtruth.npy"), outPredict)
-
+np.save((modelname + "_predictions.npy"), pred_numpy)
+np.save((modelname + "_groundtruth.npy"), outPredict)
 
 
