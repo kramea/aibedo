ce9b79b8a60e67decd0b834bad42ae5953148146
salva <salvaruehling@gmail.com>
2022-07-04 19:36:23 +0200

    add constants.py with all Climate model names

---
+++ aibedo_salva/constants.py
@@ -0,0 +1,25 @@
+CLIMATE_MODELS_ALL = [
+    "CESM2",
+    "CESM2-FV2",
+    "CESM2-WACCM",
+    "CESM2-WACCM-FV2",
+    "CMCC-CM2-SR5",
+    "CanESM5",
+    "E3SM-1-1",
+    'E3SM-1-1-ECA',
+    "FGOALS-g3",
+    "GFDL-CM4",
+    "GFDL-ESM4",
+    "GISS-E2-1-H",
+    "MIROC-ES2L",
+    "MIROC6",
+    "MPI-ESM-1-2-HAM",
+    "MPI-ESM-1-2-HR",
+    "MPI-ESM-1-2-LR",
+    "MRI-ESM2-0",
+    "SAM0-UNICON",
+
+]
+
+if __name__ == '__main__':
+    print('" "'.join(CLIMATE_MODELS_ALL))
