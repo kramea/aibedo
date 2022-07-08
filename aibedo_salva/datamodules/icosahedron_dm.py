8435d48c88e66ae5877fc118b3fccc7af478021a
salva <salvaruehling@gmail.com>
2022-07-09 01:48:05 +0200

    refactoring and fix for SunetLSTM

--- aibedo_salva/datamodules/icosahedron_dm.py
+++ aibedo_salva/datamodules/icosahedron_dm.py
@@ -16,35 +16,43 @@ class IcosahedronDatamodule(AIBEDO_DataModule):
     def __init__(self,
                  order: int = 5,
                  time_lag: int = 0,
+                 time_length=None,  # TODO: deprecate this
                  partition: Sequence[float] = (0.8, 0.1, 0.1),
                  **kwargs
                  ):
         """
         Args:
-            order (int): order of an icosahedron graph
+            order (int): order of an icosahedron graph. Either 5 or 6.
+            time_lag (int): time lag of the model.
+            time_length (int): time length of the model.
+            partition (tuple): partition of the data into train, validation and test fractions/sets.
+                Train and validation (indices 0 and 1) must be floats.
+                Test (index 2) can be a float or a string.
+                    If test is a string, it must be one of the following: 'merra2', 'era5'
+            kwargs: Additional keyword arguments for the super class (input_vars, data_dir, num_workers, batch_size,..).
         """
         super().__init__(**kwargs)
         # The following makes all args available as, e.g.: self.hparams.order, self.hparams.batch_size
         self.save_hyperparameters(ignore=[])
         self.n_pixels = icosahedron_nodes_calculator(self.hparams.order)
-        self._possible_test_sets = ['merra2', 'era5']
         # compress.isosph5.CESM2.historical.r1i1p1f1.Input.Exp8_fixed.nc
-        self._esm_name = self.hparams.input_filename.split('.')[2]
         self._check_args()
 
     def _check_args(self):
         """Check if the arguments are valid."""
-        if len(self.hparams.partition) != 3:
-            raise ValueError("partition must be a tuple of 3 values")
-        test_frac = self.hparams.partition[2]
+        partition = self.hparams.partition
+        if len(partition) != 3:
+            raise ValueError(f"partition must be a tuple of 3 values, but got {partition}, type: {type(partition)}")
+        test_frac = partition[2]
         if isinstance(test_frac, str):
             raise_error_if_invalid_value(test_frac, possible_values=self._possible_test_sets, name='partition[2]')
-            if self.hparams.partition[0] + self.hparams.partition[1] != 1:
+            if partition[0] + partition[1] != 1:
+                self.hparams.partition = (partition[0], 1 - partition[0], partition[2])
                 log.warning(
                     "partition[0] + partition[1] does not sum to 1 and test_frac is a string. partition[1] will be set to 1 - partition[0].")
-        elif self.hparams.partition[0] + self.hparams.partition[1] + self.hparams.partition[2] != 1:
+        elif partition[0] + partition[1] + partition[2] != 1:
             raise ValueError(
-                f"partition must sum to 1, but it sums to {self.hparams.partition[0] + self.hparams.partition[1] + self.hparams.partition[2]}")
+                f"partition must sum to 1, but it sums to {partition[0] + partition[1] + partition[2]}")
 
     def _concat_variables_into_channel_dim(self, data: xr.Dataset, variables: List[str], filename=None) -> np.ndarray:
         """Concatenate xarray variables into numpy channel dimension (last)."""
@@ -62,10 +70,6 @@ class IcosahedronDatamodule(AIBEDO_DataModule):
         inDS = xr.open_dataset(input_file)
         outDS = xr.open_dataset(output_file)
 
-        if hasattr(inDS, "lon"):
-            self.lon_list = inDS.lon.data
-            self.lat_list = inDS.lat.data
-
         in_vars, out_vars = self.hparams.input_vars, self.hparams.output_vars
         in_channels, out_channels = len(in_vars), len(out_vars)
 
@@ -82,19 +86,21 @@ class IcosahedronDatamodule(AIBEDO_DataModule):
         return combined_data
 
     def _model_specific_transform(self, input_data: np.ndarray, output_data: np.ndarray):
-        if "SphericalUNet" in self.model_config._target_:
-            if self.hparams.time_lag > 0:
-                input_data = input_data[:-self.hparams.time_lag]
-                output_data = output_data[self.hparams.time_lag:]
-        elif "SphericalUNet_LSTM" in self.model_config._target_:
+        if "SphericalUNetLSTM" in self.model_config._target_:
             new_in_data, new_out_data = [], []
-            time_length = self.model_config.time_length
+            time_length = self.model_config.time_len
             for i in range(0, len(input_data) - time_length):
                 intemp = np.concatenate(input_data[i:i + time_length, :, :], axis=1)
                 new_in_data.append(intemp)
                 new_out_data.append(output_data[i + time_length - 1, :, :])
 
-            input_data, output_data = np.asarray(new_in_data),  np.asarray(new_out_data)
+            input_data, output_data = np.asarray(new_in_data), np.asarray(new_out_data)
+        elif "SphericalUNet" in self.model_config._target_:
+            if self.hparams.time_lag > 0:
+                input_data = input_data[:-self.hparams.time_lag]
+                output_data = output_data[self.hparams.time_lag:]
+        else:
+            log.warning(f"No model specific transform applied for {self.model_config._target_}.")
 
         return input_data, output_data
 
@@ -107,12 +113,14 @@ class IcosahedronDatamodule(AIBEDO_DataModule):
         log.info(f"Grid level: {glevel}, # of pixels: {self.n_pixels}")
 
         if stage in ["fit", None] or test_frac not in self._possible_test_sets:
+            # compress.isosph5.SAM0-UNICON.historical.r1i1p1f1.Input.Exp8_fixed
             # E.g.:   input_file:  "<data-dir>/compress.isosph5.CESM2.historical.r1i1p1f1.Input.Exp8_fixed.nc"
             #         output_file: "<data-dir>/compress.isosph5.CESM2.historical.r1i1p1f1.Output.nc"
             fname_in = self.hparams.input_filename
             fname_out = fname_in.replace("Input.Exp8_fixed.nc", "Output.nc")
             combined_train_data = self._process_nc_dataset(fname_in, fname_out, shuffle=True)
-            train_data, val_data = train_test_split(combined_train_data, train_size=train_frac, random_state=self.hparams.seed)
+            train_data, val_data = train_test_split(combined_train_data, train_size=train_frac,
+                                                    random_state=self.hparams.seed)
 
         if test_frac in self._possible_test_sets:
             sphere = "isosph5." if 'isosph5.' in self.hparams.input_filename else "isosph."
