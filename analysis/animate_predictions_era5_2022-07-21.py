2a117f8035e89ef4f68df6058c5d0e9ab619f973
salva <salvaruehling@gmail.com>
2022-07-21 16:11:23 +0200

    small edits in animate_predictions_era5_2022-07-21.py

--- analysis/animate_predictions_era5_2022-07-21.py
+++ analysis/animate_predictions_era5_2022-07-21.py
@@ -9,21 +9,18 @@ ds_mlp = xr.load_dataset(f"{PDIR}/MLP-no-constraints-37z9auhs.nc")
 ds_sunet = xr.load_dataset(f"{PDIR}/SUNet-no-constraints-2frxrgnq.nc")
 ds_all_constraints_mlp = xr.load_dataset(f"{PDIR}/MLP-with-constraints-2ymsmghz.nc")
 
-kwargs = dict(ds=ds_mlp.isel(snapshot=slice(-6, -1)), x='longitude', y='latitude', col='snapshot')
-xr.plot.scatter(hue='pr_targets', **kwargs)
-xr.plot.scatter(hue='tas_targets', **kwargs)
-xr.plot.scatter(hue='psl_targets', **kwargs)
-vars_to_plot = 'all'
-vars_to_plot = ['tas']
+# Select which of the above datasets to plot:
 ds = ds_mlp
+
+# Select which variable to plot:
+vars_to_plot = 'all'  # alternative: ['tas']
+
 vars_to_plot = ds_mlp.variable_names.split(";") if vars_to_plot == "all" else vars_to_plot
+# Show GIFs by target variable, 1by1
 for var in vars_to_plot:
     var_id = var.replace('_targets', '').replace('_preds', '')
     print(f"Plotting {var_id}", var)
-    ds_all_constraints_mlp[var_id + '_targets'] = ds_mlp[var_id + '_targets']
+    ds[var_id + '_targets'] = ds_mlp[var_id + '_targets']
     ani1 = animate_snapshots(ds, var_to_plot=var_id + '_targets')
     ani2 = animate_snapshots(ds, var_to_plot=var_id + '_preds')
-plt.show()
-
-ani = animate_snapshots(ds_all_constraints_mlp, var_to_plot='tas_preds')
-plt.show()
+    plt.show()
