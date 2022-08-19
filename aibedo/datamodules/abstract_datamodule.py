f7f1880f06915b9b4cd279c184095bc8ad26da52
salva <salvaruehling@gmail.com>
2022-08-19 03:05:31 +0200

    implement 2D (euclidean) data module

--- aibedo/datamodules/abstract_datamodule.py
+++ aibedo/datamodules/abstract_datamodule.py
@@ -3,6 +3,8 @@ import logging
 import os
 from os.path import join
 from typing import Optional, List, Callable, Sequence, Dict
+
+from einops import rearrange, repeat
 from omegaconf import DictConfig
 import torch
 from torch import nn
@@ -16,9 +18,10 @@ import xarray as xr
 from aibedo.constants import CLIMATE_MODELS_ALL
 from aibedo.datamodules.torch_dataset import AIBEDOTensorDataset
 from aibedo.models.base_model import BaseModel
+from aibedo.skeleton_framework.data_loader import shuffle_data
 from aibedo.utilities.constraints import AUXILIARY_VARS
 from aibedo.utilities.naming import var_names_to_clean_name
-from aibedo.utilities.utils import get_logger, raise_error_if_invalid_value
+from aibedo.utilities.utils import get_logger, raise_error_if_invalid_value, get_any_ensemble_id
 
 log = get_logger(__name__)
 
@@ -78,6 +81,7 @@ class AIBEDO_DataModule(pl.LightningDataModule):
         # The following makes all args available as, e.g., self.hparams.batch_size
         self.save_hyperparameters(ignore=['model_config', 'prediction_data'])
         self.model_config = model_config
+        self._spatial_dims = dict()
         self._data_train = self._data_val = self._data_test = self._data_predict = None
         self._possible_test_sets = ['merra2', 'era5']
         self._possible_prediction_sets = self._possible_test_sets + ['val', 'same_as_test'] + CLIMATE_MODELS_ALL
@@ -91,7 +95,7 @@ class AIBEDO_DataModule(pl.LightningDataModule):
             in enumerate(self.input_var_names + ['month'] + self.hparams.auxiliary_vars)
         }
         self._var_names_to_clean_name = var_names_to_clean_name()
-        self._set_geographical_metadata()
+        # self._set_geographical_metadata()
         self._check_args()
 
     @property
@@ -100,7 +104,20 @@ class AIBEDO_DataModule(pl.LightningDataModule):
 
     @property
     def files_id(self) -> str:
-        return self.hparams.input_filename.split('.')[1]
+        return ""
+
+    @property
+    def spatial_dims(self) -> Dict[str, int]:
+        return self._spatial_dims
+
+    @property
+    def spatial_dim_names(self) -> List[str]:
+        return list(self.spatial_dims.keys())
+
+    @spatial_dims.setter
+    def spatial_dims(self, value: Dict[str, int]):
+        assert isinstance(value, dict), "spatial_dim must be a dict"
+        self._spatial_dims = value
 
     @property
     def month_index(self) -> int:
@@ -198,6 +215,176 @@ class AIBEDO_DataModule(pl.LightningDataModule):
             'land': self.land_mask(),
             'sea': self.sea_mask(),
         }
+
+    def _concat_variables_into_channel_dim(self, data: xr.Dataset, variables: List[str], filename=None) -> np.ndarray:
+        """Concatenate xarray variables into numpy channel dimension (last)."""
+        data_all = []
+        for var in variables:
+            # Get the variable from the dataset (as numpy array, by selecting .values)
+            var_data = data[var].values
+            # add feature dimension (channel)
+            var_data = np.expand_dims(var_data, axis=-1)
+            # add to list of all variables
+            data_all.append(var_data)
+
+        # Concatenate all the variables into a single array along the last (channel/feature) dimension
+        dataset = np.concatenate(data_all, axis=-1)
+        assert dataset.shape[-1] == len(variables), "Number of variables does not match number of channels."
+        return dataset
+
+    def _get_auxiliary_data(self, dataset_in: xr.Dataset, dataset_out: xr.Dataset) -> np.ndarray:
+        # Add month of the snapshot (0-11)
+        # subtract -1 because month we want 0-indexed months
+        month_of_snapshot = np.array(dataset_out.indexes['time'].month, dtype=np.float32) - 1
+        # now repeat the month for each grid cell/pixel
+        dataset_month = repeat(month_of_snapshot, f"t -> t {' '.join(self.spatial_dim_names)} ()", **self.spatial_dims)
+        # same as: np.repeat(month_of_snapshot, self.n_pixels).reshape([-1, self.n_pixels, 1])
+        if len(self.hparams.auxiliary_vars) > 0:
+            dataset_aux = self._concat_variables_into_channel_dim(dataset_out, self.hparams.auxiliary_vars)
+            dataset_aux = np.concatenate([dataset_month, dataset_aux], axis=-1)  # shape (1980, spatial-dims, 1 + #aux-vars)
+        else:
+            dataset_aux = dataset_month
+
+        return dataset_aux
+
+    def _process_nc_dataset(self,
+                            input_filename: str,
+                            output_filename: str,
+                            shuffle: bool = False, stage=None):
+        input_file = os.path.join(self.hparams.data_dir, input_filename)
+        output_file = os.path.join(self.hparams.data_dir, output_filename)
+
+        # Input data
+        in_ds = xr.open_dataset(input_file)
+        dataset_in = self._concat_variables_into_channel_dim(in_ds, self.hparams.input_vars, input_file)
+
+        # Output data
+        out_vars = self.hparams.output_vars
+        if stage == 'predict':
+            out_vars = [x.replace('_pre', '') for x in out_vars]
+            log.info(f" Using raw output data from {os.path.basename(output_file)} -- Prediction targets: {out_vars}.")
+
+        out_ds = xr.open_dataset(output_file)
+        dataset_out = self._concat_variables_into_channel_dim(out_ds, out_vars, output_file)
+
+        # Auxiliary data (e.g. output month, evaporation, ..)
+        dataset_aux = self._get_auxiliary_data(in_ds, out_ds)
+
+        # Reshape if using multiple timesteps for prediction
+        if self.window > 1:
+            window = self.window
+            log.info(f" Using {window} timesteps for prediction.")
+            in_tmp, out_tmp, aux_tmp = [], [], []
+            for i in range(0, len(dataset_in) - window):
+                # concatenate time axis into feature axis (... can stand for 1 or 2 spatial dims)
+                in_tmp += [rearrange(dataset_in[i:i + window, :, :], 't ... d -> ... (d t)')]
+                # reselect the corresponding output/aux data (at the same time as latest time step of inputs)
+                out_tmp += [dataset_out[i + window - 1, :, :]]
+                # similarly, the auxiliary data should be aligned to the last month too!
+                aux_tmp += [dataset_aux[i + window - 1, :, :]]
+
+            dataset_in = np.asarray(in_tmp, dtype=np.float32)
+            dataset_out = np.asarray(out_tmp, dtype=np.float32)
+            dataset_aux = np.asarray(aux_tmp, dtype=np.float32)
+
+        if self.hparams.time_lag > 0:
+            horizon = self.hparams.time_lag
+            # raise NotImplementedError("Time lag not implemented yet, need month for both inputs and outputs!")
+            log.info(f" Model will be forecasting {self.hparams.time_lag} time steps ahead.")
+            dataset_in = dataset_in[:-horizon, ...]
+            dataset_out = dataset_out[horizon:, ...]
+            dataset_aux = dataset_aux[horizon:, ...]
+
+        # Concatenate the auxiliary data into the input data feature dimensions (dim=last)
+        # Note, that this will not be fed into the model though -- the model inputs are cut off to only be dataset_in
+        dataset_in = np.concatenate([dataset_in, dataset_aux], axis=-1)
+        dataset_in, dataset_out = self._model_specific_transform(dataset_in, dataset_out)
+        if shuffle:
+            dataset_in, dataset_out = shuffle_data(dataset_in, dataset_out)
+
+        return dataset_in, dataset_out
+
+    def _model_specific_transform(self, input_data: np.ndarray, output_data: np.ndarray):
+        return input_data, output_data
+
+    def _get_train_and_val_data(self, stage: str, input_filename: str) -> (Tensor, Tensor, Tensor, Tensor):
+        from sklearn.model_selection import train_test_split
+        # compress.isosph5.SAM0-UNICON.historical.r1i1p1f1.Input.Exp8_fixed
+        # E.g.:   input_file:  "<data-dir>/compress.isosph5.CESM2.historical.r1i1p1f1.Input.Exp8_fixed.nc"
+        #         output_file: "<data-dir>/compress.isosph5.CESM2.historical.r1i1p1f1.Output.nc"
+        train_frac, val_frac, _ = self.hparams.partition
+        fname_out = input_filename.replace("Input.Exp8_fixed.nc", "Output.PrecipCon.nc")
+        train_data_in, train_data_out = self._process_nc_dataset(input_filename, fname_out, shuffle=True, stage=stage)
+        X_train, X_val, Y_train, Y_val = train_test_split(train_data_in, train_data_out, train_size=train_frac,
+                                                          random_state=self.hparams.seed)
+        if stage == 'predict':
+            del X_train, Y_train, train_data_in, train_data_out  # save some storage space
+            X_train = Y_train = None
+
+        return X_train, X_val, Y_train, Y_val
+
+    def _log_at_setup_start(self, stage: Optional[str] = None):
+        """Log some arguments at setup."""
+        pass
+
+    def setup(self, stage: Optional[str] = None):
+        """Load data. Set internal variables: self._data_train, self._data_val, self._data_test."""
+        raise_error_if_invalid_value(stage, ['fit', 'validate', 'test', 'predict', None], 'stage')
+        self._log_at_setup_start(stage)
+        input_filename = self.hparams.input_filename
+        train_frac, val_frac, test_frac = self.hparams.partition
+
+        if stage in ["fit", 'validate', None] or test_frac not in self._possible_test_sets:
+            X_train, X_val, Y_train, Y_val = self._get_train_and_val_data(stage, input_filename)
+
+        if stage == 'predict' and self.prediction_data in ['val'] + CLIMATE_MODELS_ALL:
+            pass
+        elif test_frac in self._possible_test_sets or (
+                stage == 'predict' and self.prediction_data in self._possible_test_sets
+        ):
+            sphere = self.files_id
+            if test_frac == 'merra2' or (stage == 'predict' and self.prediction_data == 'merra2'):
+                #                  f"compress.{sphere}MERRA2_Input_Exp8_fixed.nc"
+                test_input_fname = f"{sphere}MERRA2_Exp8_Input.2022Jul06.nc"
+                test_output_fname = f"{sphere}MERRA2_Output.2022Jul06.nc"
+            elif test_frac == 'era5' or (stage == 'predict' and self.prediction_data == 'era5'):
+                test_input_fname = f"{sphere}ERA5_Input_Exp8.nc"
+                test_output_fname = f"{sphere}ERA5_Output_PrecipCon.nc"
+            else:
+                raise ValueError(f"Unknown test_frac: {test_frac}")
+            if stage == "test" or stage == "predict":
+                X_test, Y_test = self._process_nc_dataset(test_input_fname, test_output_fname, shuffle=False,
+                                                          stage=stage)
+        else:
+            from sklearn.model_selection import train_test_split
+            X_val, X_test, Y_val, Y_test = train_test_split(X_val, Y_val, test_size=test_frac / (val_frac + test_frac),
+                                                            random_state=self.hparams.seed)
+
+        if stage in ["predict"]:
+            if self.prediction_data == 'same_as_test':
+                X_predict, Y_predict = X_test, Y_test
+            elif self.prediction_data == 'val':
+                _, X_predict, _, Y_predict = self._get_train_and_val_data(stage='predict', input_filename=input_filename)
+            elif self.prediction_data in CLIMATE_MODELS_ALL:
+                in_file_esm = get_any_ensemble_id(self.hparams.data_dir, self.prediction_data, files_id=self.files_id)
+                _, X_predict, _, Y_predict = self._get_train_and_val_data(stage='predict', input_filename=in_file_esm)
+            else:
+                raise ValueError(f"Unknown prediction_data: {self.prediction_data}")
+            self._data_predict = get_tensor_dataset_from_numpy(X_predict, Y_predict, dataset_id='predict')
+        if stage == 'fit' or stage is None:
+            self._data_train = get_tensor_dataset_from_numpy(X_train, Y_train, dataset_id='train')
+        if stage in ["fit", 'validate', None]:
+            self._data_val = get_tensor_dataset_from_numpy(X_val, Y_val, dataset_id='val')
+        if stage in ['test', None]:
+            self._data_test = get_tensor_dataset_from_numpy(X_test, Y_test, dataset_id='test')
+
+        # Data has shape (#examples, #pixels, #channels)
+        if stage in ["fit", None]:
+            log.info(f" Dataset sizes train: {len(self._data_train)}, val: {len(self._data_val)}")
+        elif stage in ["test"]:
+            log.info(f" Dataset test size: {len(self._data_test)}")
+        elif stage == 'predict':
+            log.info(f" Dataset predict size: {len(self._data_predict)}")
 
     def _shared_dataloader_kwargs(self) -> dict:
         return dict(num_workers=int(self.hparams.num_workers), pin_memory=self.hparams.pin_memory)
@@ -337,3 +524,9 @@ class AIBEDO_DataModule(pl.LightningDataModule):
             ))
         return xr_dset
 
+
+
+
+def get_tensor_dataset_from_numpy(*ndarrays, dataset_id="") -> AIBEDOTensorDataset:
+    tensors = [torch.from_numpy(ndarray).float() for ndarray in ndarrays]
+    return AIBEDOTensorDataset(*tensors, dataset_id=dataset_id)
--- aibedo/datamodules/lat_lon_dm.py
+++ aibedo/datamodules/lat_lon_dm.py
@@ -16,37 +16,27 @@ from aibedo.skeleton_framework.spherical_unet.utils.samplings import icosahedron
 log = get_logger(__name__)
 
 
-class IcosahedronDatamodule(AIBEDO_DataModule):
+class EuclideanDatamodule(AIBEDO_DataModule):
     def __init__(self,
-                 order: int = 5,
                  **kwargs
                  ):
         """
         Args:
-            order (int): order of an icosahedron graph. Either 5 or 6.
             kwargs: Additional keyword arguments for the super class (input_vars, data_dir, num_workers, batch_size,..).
         """
         super().__init__(**kwargs)
         # The following makes all args available as, e.g.: self.hparams.order, self.hparams.batch_size
         self.save_hyperparameters(ignore=[])
-        self.n_pixels = icosahedron_nodes_calculator(self.hparams.order)
-        self.spatial_dims = {'n_pixels': self.n_pixels}  # single dim for the spatial dimension
-        # compress.isosph5.CESM2.historical.r1i1p1f1.Input.Exp8_fixed.nc
+        self.spatial_dims = {'lat': 192, 'lon': 288}  # two dims for the spatial dimension
         self._check_args()
 
     @property
     def files_id(self) -> str:
-        order_s = f"isosph{self.hparams.order}" if self.hparams.order <= 5 else "isosph"
-        return f"compress.{order_s}."
+        # files are of the kind 'CESM2.historical.r1i1p1f1.Input.Exp8_fixed.nc'
+        # i.e. nothing precedes the ESM model name
+        return ""
 
     def _check_args(self):
         """Check if the arguments are valid."""
-        assert self.hparams.order in [5, 6], "Order of the icosahedron graph must be either 5 or 6."
-        assert (self.hparams.order == 5 and 'isosph5' in self.hparams.input_filename) or self.hparams.order == 6
         super()._check_args()
-
-    def _log_at_setup_start(self, stage: Optional[str] = None):
-        """Log some arguments at setup."""
-        log.info(f"Order of the icosahedron graph: {self.hparams.order}, # of pixels: {self.n_pixels}")
-        super()._log_at_setup_start()
 
