d30e3b38e9698bbbe8e376ebc4d78820d049bd24
salva <salvaruehling@gmail.com>
2022-08-24 22:26:13 +0200

    taking mean over spatial dims for constraints extended to 2D data too

--- aibedo/utilities/constraints.py
+++ aibedo/utilities/constraints.py
@@ -46,11 +46,13 @@ def precipitation_energy_budget_constraint(
     #    actual = pr + sea_surface_heat_flux[i, :] + toa_sw_net_radiation[i, :] - surface_lw_net_radiation[i, :]
     #    loss_peb += actual.mean() - PR_Err[i].mean()
     # loss_peb /= batch_size
-
+    spatial_dims = (-2, -1) if precipitation.dim() == 3 else -1  # for 2D data dim = 3, for spherical data dim=2
     pr_scaled = precipitation * 2.4536106 * 1_000_000
-    actual = (pr_scaled + sea_surface_heat_flux + toa_sw_net_radiation - surface_lw_net_radiation).mean(dim=-1)
+    actual = (
+            pr_scaled + sea_surface_heat_flux + toa_sw_net_radiation - surface_lw_net_radiation
+    ).mean(dim=spatial_dims)
     loss_peb2 = actual - PR_Err
-    return loss_peb2.mean()
+    return loss_peb2.mean()  # mean over batch dimension
 
 
 def global_moisture_constraint(precipitation: Tensor, evaporation: Tensor, PE_err: Tensor) -> float:
@@ -70,7 +72,8 @@ def global_moisture_constraint(precipitation: Tensor, evaporation: Tensor, PE_er
     #  for i in range(batch_size):
     #       loss_pr += (precipitation[i, :] - evaporation[i, :]).mean() - PE_err[i].mean()
     #  loss_pr =/ batch_size
-    err = (precipitation - evaporation).mean(dim=-1) - PE_err  # mean is over spatial dimension (1 or -1)
+    spatial_dims = (-2, -1) if precipitation.dim() == 3 else -1  # for 2D data dim = 3, for spherical data dim=2
+    err = (precipitation - evaporation).mean(dim=spatial_dims) - PE_err  # mean is over spatial dimension (1 or -1)
     return err.mean()  # return average constraint error over batch
 
 
@@ -95,7 +98,8 @@ def mass_conservation_constraint(surface_pressure: Tensor, PS_err: Tensor) -> fl
     #  for i in range(batch_size):
     #       loss_ps += surface_pressure[i, :].mean() - PS_err[i].mean()
     #  loss_ps =/ batch_size
-    err = surface_pressure.mean(dim=-1) - PS_err  # take pressure mean over the spatial dimension
+    spatial_dims = (-2, -1) if surface_pressure.dim() == 3 else -1  # for 2D data dim = 3, for spherical data dim=2
+    err = surface_pressure.mean(dim=spatial_dims) - PS_err  # take pressure mean over the spatial dimension
     return err.mean()
 
 
