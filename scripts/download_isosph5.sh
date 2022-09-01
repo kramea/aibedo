06b599f36d2f4ff2f7c674030eafb0544bff190c
salva <salvaruehling@gmail.com>
2022-09-01 18:37:35 +0200

    small update for multi-esm training

--- scripts/download_isosph5.sh
+++ scripts/download_isosph5.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 data_dir="/work/data"
-for esm in "MRI-ESM2-0" "CESM2-WACCM" "MPI-ESM-1-2-HAM" "CESM2" "GFDL-ESM4";
+for esm in "MRI-ESM2-0" "CESM2-WACCM" "MPI-ESM-1-2-HAM" "CESM2" "GFDL-ESM4" "MPI-ESM1-2-LR";
 do
   echo $esm
   aws s3 cp s3://darpa-aibedo/compress.isosph5.${esm}.historical.r1i1p1f1.Input.Exp8_fixed.nc $data_dir
   aws s3 cp s3://darpa-aibedo/compress.isosph5.${esm}.historical.r1i1p1f1.Output.PrecipCon.nc $data_dir
-  aws s3 cp s3://darpa-aibedo/isosph5.denorm_nonorm.${esm}.historical.r1i1p1f1.Output.nc $data_dir
+  aws s3 cp s3://darpa-aibedo/isosph5.denorm_nonorm.${esm}.historical.r1i1p1f1.Input.Exp8.nc $data_dir
   aws s3 cp s3://darpa-aibedo/isosph5.denorm_nonorm.${esm}.historical.r1i1p1f1.Output.nc $data_dir
 done
 
@@ -17,3 +17,5 @@ done
 
 aws s3 cp s3://darpa-aibedo/ymonmean.1980_2010.isosph5.CMIP6.historical.ensmean.Output.nc $data_dir
 aws s3 cp s3://darpa-aibedo/ymonstd.1980_2010.isosph5.CMIP6.historical.ensmean.Output.nc $data_dir
+
+#aws s3 cp s3://darpa-aibedo/isosph5.denorm_nonorm.MPI-ESM1-2-LR.historical.r1i1p1f1.Input.Exp8.nc
